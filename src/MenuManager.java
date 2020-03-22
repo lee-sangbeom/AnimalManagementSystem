082917305b4985f2e5fe5f7837361fdb197aea9d
leesb <leesb@DESKTOP-G41HNSV>
2020-03-22 18:35:06 +0900

    create menu

---
+++ src/MenuManager.java
@@ -0,0 +1,15 @@
+public class MenuManager {
+	
+		public static void main(String[] args) {
+			System.out.println("*** Animal Management System Menu ***");
+			System.out.println("1.Add Animal");
+			System.out.println("2.Delete Animal");
+			System.out.println("3.Edit Animal");
+			System.out.println("4.View Animal");
+			System.out.println("5.Exit");
+			System.out.println("Select one number between 1~5");
+		}
+		                                       
+	
+	                                       
+}
